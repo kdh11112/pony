8a25d273513f746ab16d696694c5213dba6e4e92
KDH <KDH@naver.com>
2023-07-19 16:07:17 +0900

    최초

---
+++ src/main/java/kr/co/jhta/pony/PonyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.jhta.pony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PonyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PonyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/jhta/pony/PonyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.jhta.pony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PonyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
