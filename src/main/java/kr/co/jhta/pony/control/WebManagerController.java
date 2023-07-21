0b13845bad37cde4b1153c5d5373777d348f5b7d
YG <djw2717@naver.com>
2023-07-21 10:16:07 +0900

    ..

--- src/main/java/kr/co/jhta/pony/control/WebManagerController.java
+++ src/main/java/kr/co/jhta/pony/control/WebManagerController.java
@@ -1,12 +1,15 @@
 package kr.co.jhta.pony.control;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WebManagerController {
 	@GetMapping("/notice")
-	public String notice() {
+	public String notice(Model model) {
+		
 		return "noticelist";
 	}
 	@GetMapping("/notice")
