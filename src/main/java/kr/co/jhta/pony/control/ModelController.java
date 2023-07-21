9f68870e1d0bfbbdc9c1ec8791fbe1d20c350bf3
8m8d <memory_0501@naver.com>
2023-07-21 14:47:52 +0900

    2023-07-21 14:47 hwan commit push

---
+++ src/main/java/kr/co/jhta/pony/control/ModelController.java
@@ -0,0 +1,34 @@
+package kr.co.jhta.pony.control;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.co.jhta.pony.dao.ModelDAO;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class ModelController {
+
+	@Autowired
+	ModelDAO dao;
+	
+	@RequestMapping("/modelComparison")
+	public String modelSelect(Model model) {
+		model.addAttribute("model",dao.ModelInfo());
+		return "modelComparison1";
+	}
+	
+	@GetMapping("/modelComparison2")
+	public String modelInfo(@RequestParam("selectModel1")String modelName1,@RequestParam("selectModel2")String modelName2, Model model) {
+		model.addAttribute("model1",dao.ModelOne(modelName1));
+		model.addAttribute("model2",dao.ModelOne(modelName2));
+		return "modelComparison2";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/modelComparison3.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+	<h2>${model.modelName}</h2>
+	<input type="hidden" name="selectedModel1" value="${model.modelName}">
+</body>
+</html>
