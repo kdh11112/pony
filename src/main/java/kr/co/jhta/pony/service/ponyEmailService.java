cb763ca3865c81e45873fcd6574f7fe8ea8db895
cmLee-93 <shaitan9384@gmail.com>
2023-07-24 09:06:26 +0900

    ponyEmail_주석추가

--- src/main/java/kr/co/jhta/pony/service/ponyEmailService.java
+++ src/main/java/kr/co/jhta/pony/service/ponyEmailService.java
@@ -26,8 +26,6 @@ import lombok.extern.slf4j.Slf4j;
 	public class ponyEmailService {
 
 		
-		
-		
 	    private final JavaMailSender javaMailSender;
 	    
 	    @Autowired
@@ -40,25 +38,27 @@ import lombok.extern.slf4j.Slf4j;
 	    private String id;
 
 	    
-	    
-	    public MimeMessage createMessage(String to)throws MessagingException, UnsupportedEncodingException {
-	        log.info("보내는 대상 : "+ to);
+	    //서버측에서 보낼 메세지 생성
+	    public MimeMessage createMessage(String toEmail)throws MessagingException, UnsupportedEncodingException {
+	        log.info("보내는 대상 : "+ toEmail);
 	        log.info("인증 번호 : " + ePw);
+	        
+	        //메세지 객체 생성
 	        MimeMessage message = javaMailSender.createMimeMessage();
 	        
-	        message.addRecipients(MimeMessage.RecipientType.TO, to); // to 보내는 대상
+	        message.addRecipients(MimeMessage.RecipientType.TO, toEmail); // to 보내는 대상
 	        message.setSubject("회원가입 인증 코드: "); //메일 제목
 
 	        // 메일 내용 메일의 subtype을 html로 지정하여 html문법 사용 가능
 	        String msg="";
 	        msg += "<h1 style=\"font-size: 30px; padding-right: 30px; padding-left: 30px;\">이메일 주소 확인</h1>";
-	        msg += "<p style=\"font-size: 17px; padding-right: 30px; padding-left: 30px;\">아래 확인 코드를 회원가입 화면에서 입력해주세요.</p>";
-	        msg += "<div style=\"padding-right: 30px; padding-left: 30px; margin: 32px 0 40px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">";
+	        msg += "<p style=\"font-size: 20px; padding-right: 30px; padding-left: 30px;\">아래 확인 코드를 회원가입 화면에 인증번호 입력란에 입력하고 확인버튼을 눌러주세요.</p>";
+	        msg += "<div style=\"padding-right: 30px; padding-left: 30px; margin: 30px 0 30px;\"><table style=\"border-collapse: collapse; border: 0; background-color: #F4F4F4; height: 70px; table-layout: fixed; word-wrap: break-word; border-radius: 6px;\"><tbody><tr><td style=\"text-align: center; vertical-align: middle; font-size: 30px;\">";
 	        msg += ePw;
 	        msg += "</td></tr></tbody></table></div>";
 
 	        message.setText(msg, "utf-8", "html"); //내용, charset타입, subtype
-	        message.setFrom(new InternetAddress(id,"prac_Admin")); //보내는 사람의 메일 주소, 보내는 사람 이름
+	        message.setFrom(new InternetAddress(id,"Pony")); //보내는 사람의 메일 주소, 보내는 사람 이름
 	        
 	        
 	        return message;
@@ -81,10 +81,10 @@ import lombok.extern.slf4j.Slf4j;
 	        MimeMessage 객체 안에 내가 전송할 메일의 내용을 담아준다.
 	        bean으로 등록해둔 javaMailSender 객체를 사용하여 이메일 send
 	     */
-	    public String sendSimpleMessage(String to) throws Exception {
-	        MimeMessage message = createMessage(to);
+	    public String sendSimpleMessage(String toEmail) throws Exception {
+	        MimeMessage message = createMessage(toEmail);
 	        try {
-	        	redisUtil.setDataExpire(ePw, to, 60 * 1L);
+	        	redisUtil.setDataExpire(ePw, toEmail, 60 * 1L);
 	            javaMailSender.send(message); // 메일 발송
 	        } catch (MailException es) {
 	            es.printStackTrace();
