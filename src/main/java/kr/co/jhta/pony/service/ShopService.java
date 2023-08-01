b135f58f27c0782dedfa70ee99cde270de65d594
cmLee-93 <shaitan9384@gmail.com>
2023-08-01 14:29:02 +0900

    수정중-- 주의--

--- src/main/java/kr/co/jhta/pony/service/ShopService.java
+++
@@ -1,9 +0,0 @@
-package kr.co.jhta.pony.service;
-
-import java.util.List;
-
-import kr.co.jhta.pony.dto.ShopDTO;
-
-public interface ShopService {
-	public List<ShopDTO> shopArea(String shopArea,String shopAreaPoint);
-}
